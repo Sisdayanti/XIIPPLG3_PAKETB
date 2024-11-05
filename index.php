8994a122ea2059eecf4b44d5e62570134f8c4278
Sisdayanti <sisdayanti649@gmail.com>
2024-11-05 14:24:28 +0700

    atun

--- index.php
+++ index.php
@@ -1,11 +1,45 @@
-<!DOCTYPE html>
+                                                                         <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8>
-    <meta name="viewport" content="  width=device-width, initial-scale=1"> 
-    <title>Website Galeri Foto</title>
-</head>
-<body>
-
+<meta charset="utf-8">
+<meta name="viewport"content="width=device-width,initial-scale=1">
+<title>website galeri foto</title>
+<link rel="stylesheet" type="text/css" href="assets/css/bootsrap.min.css">                                                                                                                                      
+<head>
 </body>
-</html>
+<nav class="navbar nafbar-expand-ig bg-body-tertiary">
+<div class="container">
+<a class="navbar-brand" href="#">navbar</a>
+<button class="navbar-tonggler" type="button" data-bs-tonggle="collapse" data-bs-target="#navbarNavA1tmarkup" aria-controls="navbarNavA1tmarkup" aria-expanded="false" aria-label="togle navigation">
+<span class="navbar-toggler-icon></span>
+</button>                                                                                                                                                             
+<div class="collapse navbar-collapse mt-2"id="nafbarNavA1tmarkup">
+<div class="navbar-nav me-auto">
+
+</div>
+<a href="register.php" class="btn btn-outline-primary m-1> daftar</a>
+<a href="login.php" class="btn btn-outline-succes m-1> masuk</a>                                                                                                                                                              
+</div>
+</div>
+</nav>
+
+<div class="container mt-3">
+<div class="row">
+<div class="col-md-3">
+<div class="card">
+<img src="" class="card-img-top" title="" style="height:12rem;">
+<div class="card-footer text-center">
+<a href="">10 suka </a>
+<a href="">3 komentar </a>
+</div>
+</div>
+</div>
+</div>
+</div> 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+<footer class="d-flex justify-content-center border-top mt-3 bg-light fixed-bottom">
+<p>&copy;ukk rpl 2024 | nama peserta</p>
+</footer>
+<script type="text/javascript" src="assets/js/boostrap.min.js"></script>
+</body>
+</html>                                                                                                                                                                                                      
