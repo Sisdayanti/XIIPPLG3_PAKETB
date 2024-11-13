2994b45b502e609cd26ce6d23dba693e1a9cac86
Sisdayanti <144641229+Sisdayanti@users.noreply.github.com>
2024-11-13 11:07:23 +0700

    yyn

--- index.php
+++ index.php
@@ -1,45 +1,45 @@
- <!DOCTYPE html>                                                                        <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<meta name="viewport"content="width=device-width,initial-scale=1">
-<title>website galeri foto</title>
-<link rel="stylesheet" type="text/css" href="assets/css/bootsrap.min.css">                                                                                                                                      
-<head>
-</body>
-<nav class="navbar navbar-expand-ig bg-body-tertiary">
-  <div class="container">
-     <a class="navbar-brand" href="#">navbar</a>
-     <button class="navbar-tonggler" type="button" data-bs-tonggle="collapse" data-bs-target="#navbarNavA1tmarkup" aria-controls="navbarNavA1tmarkup" aria-expanded="false" aria-label="togle navigation">
-        <span class="navbar-toggler-icon></span>
-     </button>                                                                                                                                                             
-     <div class="collapse navbar-collapse mt-2"id="navbarNavA1tmarkup">
-        <div class="navbar-nav me-auto">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Website Galeri Foto</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+</head>
+<body>
 
-        </div>
-        <a href="register.php" class="btn btn-outline-primary m-1">  daftar</a>
-        <a href="login.php" class="btn btn-outline-success m-1"> masuk</a>                                                                                                                                                              
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">Website Galeri Foto</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    </button>
+      <div class="navbar-nav me-auto">
+      
       </div>
-     </div>
-    </nav>
+      <a href="register.php" class="btn btn-outline-primary m-1">Daftar</a>
+      <a href="login.php" class="btn btn-outline-success m-1">Masuk</a>
+    </div>
+  </div>
+</nav>
 
 <div class="container mt-3">
   <div class="row">
-    <div class="col-md-3">
+    <div class="col md-3">
       <div class="card">
-        <img src="" class="card-img-top" title="" style="height:12rem;">
-         <div class="card-footer text-center">
-         <a href="">10 suka </>
-           <a href="">3 komentar </a>
-         </div>
+        <img src="" class="card-img-top" title="" style="height: 12rem;">
+        <div class="card-footer text-center">
+          <a href="">10 suka</a>
+          <a href="">27 komentar</a>
         </div>
-       </div>
       </div>
-     </div> 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+    </div>
+  </div>
+</div>
+
 <footer class="d-flex justify-content-center border-top mt-3 bg-light fixed-bottom">
-   <p>&copy;ukk rpl 2024 | nama peserta</p>
- </footer>
-<script type="text/javascript" src="assets/js/boostrap.min.js"></script>
+  <p>&copy;UKK RPL 2024 | sisda & atun</p>
+</footer>
+
+<script type="text/javascript"src="assets/js/bootstrap.min.js"></script>
 </body>
-</html>                                                                                                                                                                                                      
+</html>
